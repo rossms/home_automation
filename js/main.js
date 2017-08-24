ae8235e5c7536198bf0bc5cafb4192cc633963f1
Ross <silberquitr@gmail.com>
2017-08-23 20:04:28 -0400

    stashing work

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+function applySavings(){
+    document.getElementById('togglediv').className += ' overlay';
+    document.getElementById('confirmbox').className -= ' confirmboxhidden';
+    document.getElementById('confirmbox').className += ' confirmboxshow';
+}
